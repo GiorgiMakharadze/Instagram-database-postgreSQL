57d64369ff6518622ee0fb9139aef8979ba46d80
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-05-15 20:06:49 +0400

    added queries

---
+++ database/Queries/sugesstions.sql
@@ -0,0 +1,16 @@
+--follower suggestions for user
+WITH RECURSIVE suggestions(leader_id,follower_id, depth)AS (
+	SELECT leader_id, follower_id, 1 AS depth
+	FROM followers
+	WHERE follower_id = 1
+	UNION 
+	SELECT followers.leader_id, followers.follower_id, depth + 1
+	FROM followers
+	JOIN suggestions ON suggestions.leader_id = followers.follower_id
+	WHERE depth < 3
+)
+SELECT DISTINCT users.id, users.username
+FROM suggestions
+JOIN users ON users.id = suggestions.leader_id
+WHERE depth > 1
+LIMIT 30;
